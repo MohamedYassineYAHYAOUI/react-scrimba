3097846124d3e2cfb33bafb703f77275e77b150f
Mohamed Yassine YAHYAOUI <yassine.yahyaoui@lacombedulionvert.fr>
2022-11-10 10:09:28 +0100

    Image generator

--- Image-generator/src/componenets/Image.jsx
+++ Image-generator/src/componenets/Image.jsx
@@ -1,17 +1,39 @@
 import React from 'react'
+import { useState } from 'react';
+import data from '../ImageData'
 
 function Image() {
-    return (
-        <div className="formContent">
-            <div className="ImageForm">
-                <input className="ImageForm--TextInput" type="text" id="imageUpperText" placeholder='Upper Text' />
-                <input className="ImageForm--TextInput" type="text" id="imageLowerText" placeholder='Lower Text'/>
+    
+    const [image, setImage] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage:"http://i.imgflip.com/1bij.jpg"
+    });
+
+    const [allImages, setAllImages] = useState(data.data.memes)
+
+    function generateImage(){
+        setImage(image => 
+            ({   ...image, 
+                randomImage : allImages[Math.floor(Math.random() * allImages.length)].url
+            }));
+    }
+
+    return(
+        <div>
+            <div className="formContent">
+                <div className="ImageForm">
+                    <input className="ImageForm--TextInput" type="text" id="imageUpperText" placeholder='Upper Text' />
+                    <input className="ImageForm--TextInput" type="text" id="imageLowerText" placeholder='Lower Text'/>
+                    
+                {/* <input className="ImageForm--submitButton" type="submit" value="Get a new meme image"/> */}
+                    <button className='ImageForm--button' onClick={generateImage}>Get a new meme image 🖼</button>
+                </div>
                 
-            {/* <input className="ImageForm--submitButton" type="submit" value="Get a new meme image"/> */}
-                <button className='ImageForm--button'>Get a new meme image 🖼</button>
+                {image.randomImage && <img className='formContent--generatedImage' src={image.randomImage} alt="image generated"/>} 
+
             </div>
         </div>
-
       );
 }
 export default Image;

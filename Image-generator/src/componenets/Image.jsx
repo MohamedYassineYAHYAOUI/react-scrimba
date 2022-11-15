9f76dc8668395dfdda107a3afd34f2f1cac10eb0
Mohamed Yassine YAHYAOUI <yassine.yahyaoui@lacombedulionvert.fr>
2022-11-15 10:20:20 +0100

    l'ajout des useEffect

--- Image-generator/src/componenets/Image.jsx
+++ Image-generator/src/componenets/Image.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useEffect } from 'react';
 import { useState } from 'react';
 import data from '../ImageData'
 
@@ -10,7 +11,13 @@ function Image() {
         randomImage:"http://i.imgflip.com/1bij.jpg"
     });
 
-    const [allImages, setAllImages] = useState(data.data.memes)
+    const [allImages, setAllImages] = useState([])
+
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(res => setAllImages(res.data.memes))
+    }, [])
 
     function generateImage(){
         setImage(image => 
@@ -19,19 +26,46 @@ function Image() {
             }));
     }
 
+    function handleChange(event){
+        const {name, value} = event.target
+        setImage(imageData => ({
+            ...imageData,
+            [name] : value
+        }))
+    }
+
     return(
         <div>
             <div className="formContent">
                 <div className="ImageForm">
-                    <input className="ImageForm--TextInput" type="text" id="imageUpperText" placeholder='Upper Text' />
-                    <input className="ImageForm--TextInput" type="text" id="imageLowerText" placeholder='Lower Text'/>
-                    
-                {/* <input className="ImageForm--submitButton" type="submit" value="Get a new meme image"/> */}
+                    <input 
+                        className="ImageForm--TextInput"
+                        type="text" 
+                        id="imageUpperText" 
+                        placeholder='Upper Text' 
+                        name="topText"
+                        value={image.topText}
+                        onChange={handleChange}
+                    />
+                    <input 
+                        className="ImageForm--TextInput" 
+                        type="text" 
+                        id="imageLowerText" 
+                        placeholder='Lower Text'
+                        name="bottomText"
+                        value={image.bottomText}
+                        onChange={handleChange}
+                    />
+
                     <button className='ImageForm--button' onClick={generateImage}>Get a new meme image 🖼</button>
                 </div>
                 
-                {image.randomImage && <img className='formContent--generatedImage' src={image.randomImage} alt="image generated"/>} 
-
+                {image.randomImage &&  
+                    <div className='Image'>
+                        <img className='formContent--generatedImage' src={image.randomImage} alt="image generated"/>
+                        <h2 className="image--texte top">{image.topText}</h2>
+                        <h2 className="image--texte bottom">{image.bottomText}</h2>
+                    </div>} 
             </div>
         </div>
       );
